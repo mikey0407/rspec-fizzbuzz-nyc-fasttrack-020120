880989043e0dd990a40f27b63f7c1fea733769d6
Michael Alfaro <michael.s.alfaro@gmail.com>
2020-02-02 21:30:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
-  if int % 3 == 0 && int % 5
+  if int % 3 == 0 && int % 5==0
+    puts "fizzbuzz"
+    elsif int % 3==0 
+    puts 'fizz'
+    
